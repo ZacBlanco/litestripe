72749fe543f7c7e428a75b658f367747ec60d898
Zac Blanco <zacdblanco@gmail.com>
2016-07-16 00:36:44 -0700

    wrote setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from  setuptools import setup, find_packages
+
+setup(
+	name="litestripe",
+	version="0.0.1",
+	author="Zac Blanco",
+	author_email="no-email",
+	description="An application that makes it easy to remotely control an LED strip from your own Raspberry Pi",
+	license="GNU",
+	packages=find_packages()
+)
+
+
+
+
+
